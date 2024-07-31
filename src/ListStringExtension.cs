ac83d9f143991f5bdba1b191d85d770f9706ea86
Jake Soenneker <jake@soenneker.com>
2024-07-31 08:47:43 -0500

    comment

--- src/ListStringExtension.cs
+++ src/ListStringExtension.cs
@@ -1,6 +1,5 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace Soenneker.Extensions.List.String;
 
@@ -9,6 +8,18 @@ namespace Soenneker.Extensions.List.String;
 /// </summary>
 public static class ListStringExtension
 {
+    /// <summary>
+    /// Replaces all occurrences of a specified string in each element of the list with another specified string.
+    /// </summary>
+    /// <param name="list">The list of strings in which to replace substrings.</param>
+    /// <param name="oldValue">The string to be replaced.</param>
+    /// <param name="newValue">The string to replace all occurrences of <paramref name="oldValue"/>.</param>
+    /// <exception cref="ArgumentNullException">
+    /// Thrown when <paramref name="list"/>, <paramref name="oldValue"/>, or <paramref name="newValue"/> is <c>null</c>.
+    /// </exception>
+    /// <remarks>
+    /// This method iterates through each element in the <paramref name="list"/> and replaces all occurrences of <paramref name="oldValue"/> with <paramref name="newValue"/>.
+    /// </remarks>
     public static void Replace(this IList<string> list, string oldValue, string newValue)
     {
         for (var i = 0; i < list.Count; i++)
