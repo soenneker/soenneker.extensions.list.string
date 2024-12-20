7b58ec2f9492c0c8096fb26af574f76a1de4cb32
Jake Soenneker <jake@soeneker.com>
2024-12-20 17:34:21 +0000

    Tests

---
+++ test/Soenneker.Extensions.List.String.Tests/ListStringExtensionTests.cs
@@ -0,0 +1,13 @@
+using Soenneker.Tests.Unit;
+using Xunit;
+
+namespace Soenneker.Extensions.List.String.Tests;
+
+public class ListStringExtensionTests : UnitTest
+{
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
