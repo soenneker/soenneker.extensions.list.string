e99e405b721f13838a3d094cff01392b0a8be48b
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:02:00 -0500

    TUnit conversion

--- test/Soenneker.Extensions.List.String.Tests/ListStringExtensionTests.cs
+++ test/Soenneker.Extensions.List.String.Tests/ListStringExtensionTests.cs
@@ -1,11 +1,10 @@
 using Soenneker.Tests.Unit;
-using Xunit;
 
 namespace Soenneker.Extensions.List.String.Tests;
 
 public class ListStringExtensionTests : UnitTest
 {
-    [Fact]
+    [Test]
     public void Default()
     {
 
